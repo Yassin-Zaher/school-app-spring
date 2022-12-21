bbb4af7e251b6037584177a6b59846696c7048aa
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-12-21 17:28:14 +0100

    refactoring -v1.3

--- src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,6 @@ public class HomeController {
     public String displayHomePage() {
         return "home.html";
     }
+
 
 }
