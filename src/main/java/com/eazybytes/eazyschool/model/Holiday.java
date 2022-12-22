798536353e9fa99aa43ca59e1325e646f603b139
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-12-22 12:38:13 +0100

    refactoring -v1.4

--- src/main/java/com/eazybytes/eazyschool/model/Holiday.java
+++ src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -1,30 +1,16 @@
 package com.eazybytes.eazyschool.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
     private final String day;
     private final String event;
     private final Type type;
 
-    public String getDay() {
-        return day;
-    }
-
-    public String getEvent() {
-        return event;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
     public enum Type{
         FESTIVAL, FEDERAL
     }
 
-    public Holiday(String day, String event, Type type) {
-        this.day = day;
-        this.event = event;
-        this.type = type;
-    }
 }
