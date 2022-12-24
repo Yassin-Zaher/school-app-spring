4b2116777c981f97cfa7ad7f25b0672ae52b7599
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-12-24 13:15:24 +0100

    Server Side Validation Test 1

--- src/main/java/com/eazybytes/eazyschool/model/Contact.java
+++ src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -9,11 +9,11 @@ import lombok.Data;
 @Data
 public class Contact {
 
-    @NotBlank(message = "Name mustn't be blank")
+    @NotBlank(message = "Name must not be blank")
     @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
 
-    @NotBlank(message = "Mobile number mustn't be blank")
+    @NotBlank(message = "Mobile number must not be blank")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNum;
     @NotBlank(message = "Email must not be blank")
